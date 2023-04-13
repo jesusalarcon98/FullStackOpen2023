d60e5722da0a0a00f593a5c8e8771d8ffa76bd1c
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-04-12 18:55:01 -0600

    1.13 again complete

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
+  const [votes, setVoted] = useState(new Uint8Array(anecdotes.length));
+
+  console.log(votes);
 
   const randomAnecdote = () => {
     let random = Math.floor(Math.random() * (anecdotes.length - 0) + 0);
     setSelected(random);
   };
 
+  const addVote = () => {
+    let newVote = [...votes];
+    newVote[selected] += 1;
+    setVoted(newVote);
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={addVote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
     </div>
   );
