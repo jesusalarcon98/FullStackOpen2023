548e23ad4c2bc78a8f4ab2c1fa4228ffa56dbf85
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-04-13 19:14:40 -0600

    Exercise 2.2 complete

--- Part2/courseinfo/components/Course.jsx
+++ Part2/courseinfo/components/Course.jsx
@@ -3,12 +3,16 @@ import Header from "../components/Header";
 import CourseContent from "../components/CourseContent";
 
 const Course = ({ course }) => {
+  let total = 0;
+
   return (
     <div>
       <Header header={course.name} />
       {course.parts.map((courses) => {
+        total = courses.exercises + total;
         return <CourseContent key={courses.id} parts={courses} />;
       })}
+      <b>Total of {total} exercises</b>
     </div>
   );
 };
