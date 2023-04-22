81ebe4e51e11e71550a3e1448f2b006b9a665891
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-04-22 17:02:28 -0600

    exercise 2.6 complete

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const changeText = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const person = {
+      name: newName,
+    };
+    setPersons(persons.concat(person));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          <input value={newName} onChange={changeText} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name}>{person.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
