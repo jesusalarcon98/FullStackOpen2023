34ec0e448f2c84bc6ded80bb935d880ce8d5837f
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-04-25 13:22:38 -0600

    Part 2.c complete

--- Part2/notes/src/App.jsx
+++ Part2/notes/src/App.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import Note from "../components/Note";
+import axios from "axios";
+import { useEffect } from "react";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value);
   };
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
