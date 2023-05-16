0f2dd205e52054d2851edba234919d75cb26c2e4
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-05-15 22:15:56 -0600

    Exercises 3.3-3.6 complete

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
+    name: "Ada Lovelace",
+    number: "928-301-234",
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
@@ -31,10 +29,52 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
   <p>${fullDate}</p>
   `);
-  /* app.get("/", (request, response) => {
-  response.send("<h1>adioooos World!</h1>");
-}); */
 });
+
+app.get(`/api/persons/:id`, (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete(`/api/persons/:id`, (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const doubleName = persons.find((person) => person.name === body.name);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  if (doubleName !== undefined && body.name === doubleName.name) {
+    return response.status(404).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
