5e24025309b2b75336ede2f1e0d7de42bc72c130
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-05-17 17:05:18 -0600

    Part 3B complete

--- Part2/phonebook/components/Notification.jsx
+++ Part2/phonebook/components/Notification.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 const Notification = ({ message }) => {
-  console.log(message);
   if (!message) {
     return null;
   }
-
   return <div className={message.type}>{message.message}</div>;
 };
 
