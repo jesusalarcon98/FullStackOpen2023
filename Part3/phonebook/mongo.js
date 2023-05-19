e32cc44db4c16024c2fb505b24ba30056897ac54
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-05-19 17:37:59 -0600

    exercise 3.12 complete

---
+++ Part3/phonebook/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://JesusAlarcon:${password}@clusterprueba.psxsc82.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+
+  /*  id: 1,
+    name: "Ada Lovelace",
+    number: "928-301-234", */
+});
+
+const Person = mongoose.model("Person", noteSchema);
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+if (process.argv.length > 3) {
+  person.save().then((result) => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
