1201ea3f67f9c7c6d23623f90446f97e5cb35280
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-05-20 00:42:22 -0600

    Part 3.c middle

---
+++ Part3/exercises/models/note.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const password = process.env.VITE_MONGODB_KEY;
+const url = `mongodb+srv://JesusAlarcon:${password}@clusterprueba.psxsc82.mongodb.net/notes?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+
+module.exports = mongoose.model("Note", noteSchema);
