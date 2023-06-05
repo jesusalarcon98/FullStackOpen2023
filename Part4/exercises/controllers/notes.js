d26015fd05ecf705298e75e3efd8af9c4180935f
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-06-04 19:57:03 -0600

    Create token

--- Part4/exercises/controllers/notes.js
+++ Part4/exercises/controllers/notes.js
@@ -1,6 +1,17 @@
 const notesRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 const Note = require("../models/note");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    console.log("dentro", authorization.substring(7));
+    return authorization.substring(7);
+  }
+  console.log("dentr de else");
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
   const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
@@ -16,29 +27,28 @@ notesRouter.get("/:id", async (request, response) => {
   }
 });
 
-notesRouter.post("/", async (request, response, next) => {
-  const { body } = request;
-
-  const { content, important, userId } = body;
-
-  const user = await User.findById(userId);
+notesRouter.post("/", async (request, response) => {
+  const body = request.body;
+  const token = getTokenFrom(request);
+  console.log("dentro de post y vemos el token", token);
+  const decodedToken = jwt.verify(token, process.env.VITE_TOKEN_KEY);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
-    content,
-    important: important === undefined ? false : important,
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
     date: new Date(),
     user: user._id,
   });
 
-  try {
-    const savedNote = await note.save();
-    user.notes = user.notes.concat(savedNote._id);
-    await user.save();
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
 
-    response.json(savedNote);
-  } catch (error) {
-    next(error);
-  }
+  response.json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response) => {
