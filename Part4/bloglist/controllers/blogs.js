1604c27e8990f6aa5dc26270bfc42ccf1f62d898
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-06-06 21:54:05 -0600

    Exercise 4.21 complete

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -41,6 +41,17 @@ blogsRouter.post("/", async (request, response, next) => {
 
 blogsRouter.delete("/:id", async (request, response, next) => {
   try {
+    const decodedToken = jwt.verify(request.token, process.env.VITE_TOKEN_KEY);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const blog = await Blog.findById(request.params.id);
+    if (!blog) {
+      return response.status(404).json({ error: "blog not found" });
+    }
+    if (blog.user.toString() !== decodedToken.id) {
+      return response.status(403).json({ error: "unauthorized access" });
+    }
     await Blog.findByIdAndRemove(request.params.id);
     response.status(204).end();
   } catch (exception) {
