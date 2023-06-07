0b84eaddc7cfd27ad1aed0ad8bb92c7e6c6345b1
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-06-06 20:56:40 -0600

    Exercise 4.20 complete

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
@@ -19,32 +11,31 @@ blogsRouter.get("/", (request, response) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
-
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.VITE_TOKEN_KEY);
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ? body.likes : 0,
-    user: user._id,
-  });
-
-  if (!body.title || !body.url) {
-    return response.status(400).json({ error: "title and url are required" });
-  }
   try {
+    const decodedToken = jwt.verify(request.token, process.env.VITE_TOKEN_KEY);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ? body.likes : 0,
+      user: user._id,
+    });
+
+    if (!body.title || !body.url) {
+      return response.status(400).json({ error: "title and url are required" });
+    }
+
     const blogSaved = await blog.save();
     user.blogs = user.blogs.concat(blogSaved._id);
     await user.save();
     response.json(blogSaved);
-  } catch (exception) {
-    next(exception);
+  } catch (error) {
+    next(error);
   }
 });
 
--- Part4/bloglist/utils/middleware.js
+++ Part4/bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -12,6 +14,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    request.token = authorization.substring(7);
+    return next();
+  }
+  request.token = null;
+  return next();
+};
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
@@ -20,6 +32,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
+    console.log("hola mundo");
     return response.status(400).json({ error: error.message });
   } else if (error.name === "TokenExpiredError") {
     return response.status(401).json({
@@ -33,5 +46,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
+  tokenExtractor,
   errorHandler,
 };
