94ceeda06c98b85a510a094c599c070cef816926
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-06-29 19:58:30 -0600

    Exercise 5.7 show extra info blog complete

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -117,7 +117,7 @@ const App = () => {
         </Togglable>
 
         {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} setErrorMessage={setErrorMessage} />
+          <Blog key={blog.id} blog={blog} buttonLabel="View" />
         ))}
       </div>
     );
--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog, buttonLabel }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={toggleVisibility}>hide</button><br></br>
+        {blog.url}<br></br>
+        likes {blog.likes}<br></br>
+        {blog.author}
+
+
+
+
+      </div>
+    </div>
+  )
+}
 
 export default Blog
