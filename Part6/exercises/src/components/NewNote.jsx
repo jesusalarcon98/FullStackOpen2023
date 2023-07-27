1afaeaa6aae4a4ef08be1ea31a8785382792c042
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-07-26 23:00:24 -0600

    exercise 6.1-6.2 unicafe counter complete

--- Part6/exercises/src/components/NewNote.jsx
+++ Part6/exercises/src/components/NewNote.jsx
@@ -1,22 +1,73 @@
-import { useDispatch } from 'react-redux'
-import { createNote } from '../reducers/noteReducer'
+import { useDispatch } from "react-redux";
+import { createNote } from "../reducers/noteReducer";
 
 const NewNote = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(createNote(content))
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    dispatch(createNote(content));
+  };
 
   return (
     <form onSubmit={addNote}>
-      <input name="note" />
-      <button type="submit">add</button>
+      <input name='note' />
+      <button type='submit'>add</button>
     </form>
+  );
+};
+
+export default NewNote;
+
+/* import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button 
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button 
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
   )
 }
 
-export default NewNote
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp) */
