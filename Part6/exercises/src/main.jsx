2713545e862c47237ba794e1395533ae1f71d37b
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-07-27 22:02:00 -0600

    6.a complete

--- Part6/exercises/src/main.jsx
+++ Part6/exercises/src/main.jsx
@@ -7,7 +7,10 @@ import { Provider } from "react-redux";
 import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 
-const store = createStore(noteReducer);
+const store = createStore(
+  noteReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
