b3426635004e64511ef78532ef4b0f5b39472938
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-08-11 09:20:09 -0600

    Part 6.c redux-thunk complete

--- Part6/exercises/src/main.jsx
+++ Part6/exercises/src/main.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
-import filterReducer from "./reducers/filterReducer";
-import noteReducer from "./reducers/noteReducer";
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
+import store from "./components/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
