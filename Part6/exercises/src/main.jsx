b6964dc62e65f91078cf5cfe51a3d9a0e17f0f8a
jesusalarcon98 <jesus.alarconfrias@hotmail.com>
2023-08-10 19:01:51 -0600

    Create new note with service

--- Part6/exercises/src/main.jsx
+++ Part6/exercises/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
-
-import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
+import noteReducer from "./reducers/noteReducer";
 
 const store = configureStore({
   reducer: {
@@ -13,8 +12,6 @@ const store = configureStore({
     filter: filterReducer,
   },
 });
-
-console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
